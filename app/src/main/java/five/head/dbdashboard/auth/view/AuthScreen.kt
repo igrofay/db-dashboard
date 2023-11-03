beb9a91dd53a02b06e40c66b5216dd38c7d819b7
sergejnikitin <igrofay@gmail.com>
2023-11-03 20:18:16 +0500

    create

--- app/src/main/java/five/head/dbdashboard/auth/view/AuthScreen.kt
+++ app/src/main/java/five/head/dbdashboard/auth/view/AuthScreen.kt
@@ -1,11 +1,35 @@
 package five.head.dbdashboard.auth.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import five.head.dbdashboard.R
 import five.head.dbdashboard.auth.view_model.AuthVM
 import five.head.dbdashboard.common.view_model.rememberVM
+import five.head.dbdashboard.common.view.button.alphaClick
 
 @Composable
 fun AuthScreen() {
@@ -13,8 +37,59 @@ fun AuthScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-
+            .background(MaterialTheme.colors.primary)
     ){
-
+        Icon(
+            painter = painterResource(R.drawable.ic_logo_two),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.TopCenter),
+            tint = Color.White
+        )
+        Column(
+            modifier = Modifier
+                .align(Alignment.Center),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_logo),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth(0.7f),
+                tint = Color.White
+            )
+            Spacer(modifier = Modifier.height(35.dp))
+            Text(
+                text = "${stringResource(R.string.welcome)}!",
+                fontSize = 25.sp,
+                fontWeight = FontWeight.W700,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .alphaClick {  }
+                    .background(Color.White, CircleShape)
+                    .padding(horizontal = 22.dp, vertical = 13.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement
+                    .spacedBy(8.dp)
+            ){
+                Image(
+                    painter = painterResource(R.drawable.ic_logo_telega),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(20.dp)
+                )
+                Text(
+                    text = stringResource(R.string.login_via_telegram),
+                    fontWeight = FontWeight.W400,
+                    fontSize = 14.sp,
+                    color = MaterialTheme.colors.primary
+                )
+            }
+        }
     }
 }
